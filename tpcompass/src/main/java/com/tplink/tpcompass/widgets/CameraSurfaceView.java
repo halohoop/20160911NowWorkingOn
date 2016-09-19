55931a65b55305b6704345b7911e97c268153037
Huang Haiqi <huanghaiqi@tp-link.net>
2016-09-19 15:29:10 +0800

    加入调用摄像头逻辑； 但是高版本用不了； add  a file;

---
+++ tpcompass/src/main/java/com/tplink/tpcompass/widgets/CameraSurfaceView.java
@@ -0,0 +1,33 @@
+package com.tplink.tpcompass.widgets;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class CameraSurfaceView extends SurfaceView
+        implements SurfaceHolder.Callback {
+    private static final String TAG = "COMPASS_APP_" + CameraSurfaceView.class.getSimpleName();
+    private SurfaceHolder mHolder = getHolder();
+
+    public CameraSurfaceView(Context paramContext, AttributeSet paramAttributeSet) {
+        super(paramContext, paramAttributeSet);
+        this.mHolder.addCallback(this);
+        this.mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
